85733a1bd7dfa8060466fcc452ff45d514ab1fa0
blaimuma <135834993+blaimuma@users.noreply.github.com>
2023-06-18 01:35:08 +0200

    Update img.js

--- img.js
+++ img.js
@@ -1,13 +1,10 @@
-function copyText(event) {
-    event.preventDefault()
-    const textToCopy = "blaimuma22@gmail.com";
-    navigator.clipboard.writeText(textToCopy)
-      .then(() => {
-        console.log("Texto copiado al portapapeles: " + textToCopy);
-        alert("Texto copiado al portapapeles: " + textToCopy);
-      })
-      .catch((error) => {
-        console.error("Error al copiar el texto: " + error);
-        alert("Error al copiar el texto: " + error);
-      });
-  }
+const textToCopy = "blaimuma22@gmail.com";
+navigator.clipboard.writeText(textToCopy)
+  .then(() => {
+    console.log("Texto copiado al portapapeles: " + textToCopy);
+    alert("Texto copiado al portapapeles: " + textToCopy);
+  })
+  .catch((error) => {
+    console.error("Error al copiar el texto: " + error);
+    alert("Error al copiar el texto: " + error);
+  });
